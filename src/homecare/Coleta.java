c2b5a36d88bb72823402570e291d3736bbcd338b
Abilio Gambim Parada <abiliogp@localhost.localdomain>
2013-10-10 17:05:34 -0300

    suporte a client multithreading

--- src/homecare/Coleta.java
+++ src/homecare/Coleta.java
@@ -2,101 +2,109 @@ package homecare;
 
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Coleta {
 
-	private ArrayList<HomeCare> homeCare;
+	private ArrayList<PrintStream> coletas;
 
 	private ServerSocket server;
-	private Socket connection;
-	private ObjectInputStream input;
-	private ObjectOutputStream output;
+	
+
+	private InputStream input;
+	private OutputStream output;
 	private int counter = 1;
+
+	public Coleta(int porta) throws IOException {
+		server = new ServerSocket(porta);
+		coletas = new ArrayList<PrintStream>();
+		
+	}
 
 	public void getDadosHomeCare() {
 
 	}
 
-	public void getRedundancia() {
+	public void runServer() {
 		while (true) {
 			try {
-				server = new ServerSocket(12345, 100);
 				waitForConnection();
-				getStreams();
-				processConnection();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} finally {
-				closeConnection();
 				++counter;
 			}
 		}
 	}
 
+	
 	private void waitForConnection() throws IOException {
-		connection = server.accept();
+		Socket client = server.accept();
 		System.out.println("Connection " + counter + " received from: "
-				+ connection.getInetAddress().getHostName());
+				+ client.getInetAddress().getHostName());
+		
+		PrintStream ps = new PrintStream(client.getOutputStream());
+	       this.coletas.add(ps);
+		
+		ClientConnection clientConnection = new ClientConnection(client.getInputStream());
+		new Thread(clientConnection).start();
+		
 	}
 
-	private void getStreams() throws IOException {
-		output = new ObjectOutputStream(connection.getOutputStream());
-		output.flush();
+	/*
+	 * Suporte à vários clientes conectados ao servidor
+	 */
+	private class ClientConnection implements Runnable {
 
-		input = new ObjectInputStream(connection.getInputStream());
-	}
+		private InputStream input;
 
-	private void processConnection() {
-		String message = null;
-		sendData();
-		do {
-			try {
-				message = (String) input.readObject();
-				System.out.println("Client: " + message);
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		public ClientConnection(InputStream input) {
+			this.input = input;
+		}
+
+		public void run() {
+			broadCast();
+			Scanner s = new Scanner(this.input);
+			while (s.hasNextLine()) {
+				//recebe informações dos outros clientes
+				System.out.println(s);
 			}
-		} while (!message.equals("client>>end"));
-	}
-
-	private void sendData() {
-		try {
-			output.writeObject("Server: "
-					+ server.getInetAddress().getHostName());
-			output.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
+			s.close();
 		}
 	}
 
-	private void closeConnection() {
-		try {
-			output.close();
-			input.close();
-			connection.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
+	/*
+	 * Envia as informações do servidor para todos os clientes
+	 */
+	public void broadCast() {
+		 for (PrintStream coleta : this.coletas) {
+		       //home.println(msg);
+			 coleta.println("oi " +  this.server.getInetAddress().getHostName());
+		 }
+
 	}
 	
 	
-	public static void main(String[] args) throws UnknownHostException, IOException{
-		Coleta coleta = new Coleta();
-		coleta.getRedundancia();
-		Socket client = new Socket(InetAddress.getByName("127.0.0.1"), 12345);
-		
+
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+		Coleta coleta = new Coleta(12345);
+		for(int i = 0; i<1; i++){
+			Socket cliente = new Socket("127.0.0.1", 12345);
+			
+		}
+		coleta.runServer();
 	}
 }
